9f884b428f52da5de68dd738bac8456c36573a55
Vasiliy Kirichenko <131616200+VaskaDaGama26@users.noreply.github.com>
2025-04-23 18:23:53 +0400

    hotfix(MainPage): fixed padding;

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -34,7 +34,7 @@ const MainPage = () => {
       <div className="max-w-7xl mx-auto flex flex-col gap-8 items-center">
         <Works />
       </div>
-      <div className="max-w-7xl mx-auto w-full flex flex-col gap-8 mb-16 px-4">
+      <div className="max-w-7xl mx-auto w-full flex flex-col gap-8 mt-16 px-4">
         <Studios />
       </div>
     </>
